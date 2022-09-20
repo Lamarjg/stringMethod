992b73b3ae5d4e4d838ce4597f64d7794fc73f3d
Jordan Lamar <jordanlamar153@gmail.com>
2022-09-20 18:49:02 -0400

    first commit

---
+++ Main.java
@@ -0,0 +1,14 @@
+package javaVariable;
+
+public class Main {
+	
+	public static void main(String[] args) {
+		String myStr = "Hello";
+		System.out.println(myStr.startsWith("Hel"));   // true
+		System.out.println(myStr.startsWith("llo"));   // false
+		System.out.println(myStr.startsWith("o"));     // false
+		
+	}	
+
+
+}
